0f68f9ef6e5ea0f7d41a1dbbe7bf926ed62cd4dd
zhangxiang <958033967@qq.com>
2018-07-17 23:23:49 +0800

    add

--- SQL_exercise_01/1_questions.sql
+++ SQL_exercise_01/1_questions.sql
@@ -6,8 +6,11 @@ SELECT name, prices FROM Products;
 -- 1.3 Select the name of the products with a price less than or equal to $200.
 SELECT name FROM Products WHERE prices <= 200;
 -- 1.4 Select all the products with a price between $60 and $120.
+SELECT * FROM Products WHERE prices BETWEEN 60 AND 120;
 -- 1.5 Select the name and price in cents (i.e., the price must be multiplied by 100).
+
 -- 1.6 Compute the average price of all the products.
+SELECT 
 -- 1.7 Compute the average price of all products with manufacturer code equal to 2.
 -- 1.8 Compute the number of products with a price larger than or equal to $180.
 -- 1.9 Select the name and price of all products with a price larger than or equal to $180, and sort first by price (in descending order), and then by name (in ascending order).
