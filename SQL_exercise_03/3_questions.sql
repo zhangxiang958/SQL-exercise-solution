461a9c832f612052e67eed4ef1e2611fe8ed0523
zhangxiang <958033967@qq.com>
2018-08-11 23:35:52 +0800

    3

--- SQL_exercise_03/3_questions.sql
+++ SQL_exercise_03/3_questions.sql
@@ -1,13 +1,21 @@
 -- The Warehouse
 -- lINK: https://en.wikibooks.org/wiki/SQL_Exercises/The_warehouse
 --3.1 Select all warehouses.
+SELECT * FROM warehouses;
 --3.2 Select all boxes with a value larger than $150.
+SELECT * FROM boxes WHERE value > 150;
 --3.3 Select all distinct contents in all the boxes.
+SELECT DISTINCT contents FROM boxes;
 --3.4 Select the average value of all the boxes.
+SELECT AVG(value) FROM boxes;
 --3.5 Select the warehouse code and the average value of the boxes in each warehouse.
+SELECT warehouses, AVG(value) FROM boxes GROUP BY warehouses;
 --3.6 Same as previous exercise, but select only those warehouses where the average value of the boxes is greater than 150.
+SELECT warehouses, AVG(value) FROM boxes GROUP BY warehouses HAVING AVG(value) > 160;
 --3.7 Select the code of each box, along with the name of the city the box is located in.
+SELECT warehouses.location, boxes.code FROM warehouses, boxes WHERE boxes.warehouses = warehouses.code;
 --3.8 Select the warehouse codes, along with the number of boxes in each warehouse. 
+SELECT 
     -- Optionally, take into account that some warehouses are empty (i.e., the box count should show up as zero, instead of omitting the warehouse from the result).
 --3.9 Select the codes of all warehouses that are saturated (a warehouse is saturated if the number of boxes in it is larger than the warehouse's capacity).
 --3.10 Select the codes of all the boxes located in Chicago.
