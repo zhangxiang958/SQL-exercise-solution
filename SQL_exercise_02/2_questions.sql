fc2d35ad77f740ea6c6c95498f64b6f33df17579
Shawn Cheung <958033967@qq.com>
2018-08-11 16:57:49 +0800

    2

--- SQL_exercise_02/2_questions.sql
+++ SQL_exercise_02/2_questions.sql
@@ -28,11 +28,22 @@ SELECT * FROM department WHERE budget > (SELECT AVG(budget) FROM department);
 -- 2.14 Select the names of departments with more than two employees.
 SELECT name FROM (SELECT COUNT(*) AS employeesCount, department FROM employees GROUP BY department) count WHERE employeesCount > 2;
 -- 2.15 Very Important - Select the name and last name of employees working for departments with second lowest budget.
-SELECT 
+SELECT name, lastname FROM employees, department 
+WHERE employees.department IN 
+(SELECT code FROM department ORDER BY budget ASC HAVING budget = 
+ (SELECT budget FROM department ORDER BY budget ASC LIMIT 1, 1)
+);
 -- 2.16  Add a new department called "Quality Assurance", with a budget of $40,000 and departmental code 11. 
+INSERT INTO department VALUES(11, 'Quality ASssurance', 40000);
 -- And Add an employee called "Mary Moore" in that department, with SSN 847-21-9811.
+INSERT INTO employees VALUES('847-21-9811', 'Mary', 'Moore', 11);
 -- 2.17 Reduce the budget of all departments by 10%.
+UPDATE department SET budget = budget * 0.9;
 -- 2.18 Reassign all employees from the Research department (code 77) to the IT department (code 14).
+UPDATE employees SET department = 14 WHERE department = 77;
 -- 2.19 Delete from the table all employees in the IT department (code 14).
+DELETE FROM employees WHERE department = 14;
 -- 2.20 Delete from the table all employees who work in departments with a budget greater than or equal to $60,000.
+DELETE FROM employees WHERE department IN (SELECT code FROM department WHERE budget >= 60000);
 -- 2.21 Delete from the table all employees.
+DELETE FROM employees;
