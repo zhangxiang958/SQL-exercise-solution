62fa498d84fcac8f8e182b4246dac24404426c9e
zhangxiang958 <958033967@qq.com>
2018-08-18 20:47:12 +0800

    6

--- SQL_exercise_06/6_questions.sql
+++ SQL_exercise_06/6_questions.sql
@@ -2,4 +2,12 @@
 -- 6.1 List all the scientists' names, their projects' names, 
     -- and the hours worked by that scientist on each project, 
     -- in alphabetical order of project name, then scientist name.
+SELECT scientists.name, projects.name
+FROM assignedTo, scientists, projects
+WHERE assignedTo.scientists = scientists.SSN AND assignedTo.project = projects.code
+ORDER BY projects.name, scientists.name;
+
 -- 6.2 Select the project names which are not assigned yet
+SELECT name
+FROM projects
+WHERE code NOT IN (SELECT project FROM assignedTo);
