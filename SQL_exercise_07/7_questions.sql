1692c6e6f301380cdfc690e63634d14551dc894e
zhangxiang958 <958033967@qq.com>
2018-08-18 20:48:10 +0800

    7

--- SQL_exercise_07/7_questions.sql
+++ SQL_exercise_07/7_questions.sql
@@ -1,4 +1,12 @@
 -- https://en.wikibooks.org/wiki/SQL_Exercises/Planet_Express 
 -- 7.1 Who receieved a 1.5kg package?
     -- The result is "Al Gore's Head".
+SELECT client.name
+FROM client join package
+ON client.accountNumber = package.recipient
+WHERE package.weight = 1.5;
+
+
 -- 7.2 What is the total weight of all the packages that he sent?
+SELECT SUM(weight)
+FROM client join package ON client.accountNumber = package.recipient AND client.name = 'Al Gore's Head';
